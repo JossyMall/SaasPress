a005cfa2e4162d31a7e40a5d2b7e131f7e497fba
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-06-26 22:49:29 -0700

    Update class-saaspress.php

--- includes/class-saaspress.php
+++ includes/class-saaspress.php
@@ -1,23 +1,35 @@
 <?php
-class SaaasPress {
-    private $tenant_manager;
-
-    public function __construct($tenant_manager) {
-        $this->tenant_manager = $tenant_manager;
-
-        add_action('init', [$this, 'apply_global_filters']);
-        add_action('user_register', [$this, 'make_new_user_tenant']);
+class SaaSPress {
+    public static function init() {
+        // Register hooks
+        add_action('admin_menu', array(__CLASS__, 'register_admin_pages'));
     }
 
-    public function apply_global_filters() {
-        $global_filters = get_option('saaspress_global_filters', array());
-        $this->tenant_manager->apply_filters($global_filters);
+    public static function register_admin_pages() {
+        add_menu_page('SaaSPress', 'SaaSPress', 'manage_options', 'saaspress', array(__CLASS__, 'about_page'), 'dashicons-admin-multisite');
+        add_submenu_page('saaspress', 'About Us', 'About Us', 'manage_options', 'saaspress-about', array(__CLASS__, 'about_page'));
+        add_submenu_page('saaspress', 'Tenants', 'Tenants', 'manage_options', 'saaspress-tenants', array(__CLASS__, 'tenants_page'));
+        add_submenu_page('saaspress', 'Configurations', 'Configurations', 'manage_options', 'saaspress-config', array(__CLASS__, 'config_page'));
+        add_submenu_page('saaspress', 'Global Filters', 'Global Filters', 'manage_options', 'saaspress-global-filters', array(__CLASS__, 'global_filters_page'));
     }
 
-    public function make_new_user_tenant($user_id) {
-        $global_filters = get_option('saaspress_global_filters', array());
-        $db_details = get_option('saaspress_default_db_details', array());
-        $this->tenant_manager->make_tenant($user_id, $global_filters, $db_details['db_host'], $db_details['db_name'], $db_details['db_user'], $db_details['db_pass']);
+    public static function about_page() {
+        echo '<div class="wrap"><h1>About SaaSPress</h1><p>This plugin allows for a multi-tenant setup within WordPress, providing unique content and isolated environments for each user.</p></div>';
+    }
+
+    public static function tenants_page() {
+        require_once plugin_dir_path(__FILE__) . '../admin/saaspress-tenants-page.php';
+        saaspress_tenants_page();
+    }
+
+    public static function config_page() {
+        require_once plugin_dir_path(__FILE__) . '../admin/saaspress-config-page.php';
+        saaspress_config_page();
+    }
+
+    public static function global_filters_page() {
+        require_once plugin_dir_path(__FILE__) . '../admin/saaspress-global-filters.php';
+        saaspress_global_filters_page();
     }
 }
 ?>
