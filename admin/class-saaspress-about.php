195f431d0288f1da371fdf7d43cc02673201100b
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-06-29 18:36:53 -0700

    Create class-saaspress-about.php

---
+++ admin/class-saaspress-about.php
@@ -0,0 +1,4 @@
+<div class="wrap">
+    <h1>About SaaSPress</h1>
+    <p>SaaSPress is a plugin that allows you to create a multi-tenant setup within WordPress. Each user has their own isolated environment, including unique content and independent plugin instances, all managed under a single WordPress installation.</p>
+</div>
