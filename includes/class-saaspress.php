33aa54a6a2442f2c177fadf44c0e78a26ebc09bc
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-04 00:28:58 -0700

    Update class-saaspress.php

--- includes/class-saaspress.php
+++ includes/class-saaspress.php
@@ -4,49 +4,25 @@ class SaasPress {
 
     public function __construct($tenant_manager) {
         $this->tenant_manager = $tenant_manager;
-        add_action('admin_menu', [$this, 'add_admin_pages']);
-        add_filter('query', [$this, 'filter_queries']);
+
+        add_filter('query', [$this, 'filter_query']);
     }
 
-    public function add_admin_pages() {
-        add_menu_page('SaaS Press', 'SaaS Press', 'manage_options', 'saaspress', [$this, 'saaspress_global_filters_page']);
-        add_submenu_page('saaspress', 'Global Filters', 'Global Filters', 'manage_options', 'saaspress-global-filters', [$this, 'saaspress_global_filters_page']);
-        add_submenu_page('saaspress', 'Configurations', 'Configurations', 'manage_options', 'saaspress-configurations', [$this, 'saaspress_configurations_page']);
-        add_submenu_page('saaspress', 'Tenants', 'Tenants', 'manage_options', 'saaspress-tenants', [$this, 'saaspress_tenants_page']);
-        add_submenu_page('saaspress', 'About Us', 'About Us', 'manage_options', 'saaspress-about', [$this, 'saaspress_about_page']);
-    }
-
-    public function saaspress_global_filters_page() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            update_option('saaspress_global_filters', $_POST['saaspress_global_filters']);
-            update_option('saaspress_databases', $_POST['saaspress_databases']);
-            update_option('saaspress_tenant_limit_per_db', $_POST['saaspress_tenant_limit_per_db']);
-        }
-        include plugin_dir_path(__FILE__) . 'admin/global-filters.php';
-    }
-
-    public function saaspress_configurations_page() {
-        include plugin_dir_path(__FILE__) . 'admin/configurations.php';
-    }
-
-    public function saaspress_tenants_page() {
-        include plugin_dir_path(__FILE__) . 'admin/tenants.php';
-    }
-
-    public function saaspress_about_page() {
-        include plugin_dir_path(__FILE__) . 'admin/about.php';
-    }
-
-    public function filter_queries($query) {
+    public function filter_query($query) {
         global $wpdb;
         $current_user = wp_get_current_user();
-        $tenant_prefix = get_user_meta($current_user->ID, 'saaspress_tenant_prefix', true);
-        if ($tenant_prefix) {
-            $global_filters = get_option('saaspress_global_filters', []);
-            foreach ($global_filters as $table) {
-                $query = str_replace($wpdb->prefix . $table, $wpdb->prefix . $tenant_prefix . $table, $query);
+
+        if ($this->tenant_manager->is_tenant($current_user->ID)) {
+            $tenant_prefix = $this->tenant_manager->get_tenant_prefix($current_user->ID);
+
+            $tables = $this->tenant_manager->get_global_filtered_tables();
+            foreach ($tables as $table) {
+                $base_table = $wpdb->prefix . $table;
+                $tenant_table = $tenant_prefix . $table;
+                $query = str_replace($base_table, $tenant_table, $query);
             }
         }
+
         return $query;
     }
 }
