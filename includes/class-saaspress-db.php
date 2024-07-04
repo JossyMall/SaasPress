b7ff6ac3b14332c5f4608b55d17173bde3fa9bb0
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-04 00:28:02 -0700

    Update class-saaspress-db.php

--- includes/class-saaspress-db.php
+++ includes/class-saaspress-db.php
@@ -1,1 +1,21 @@
+<?php
+class SaasPress_DB {
+    private $wpdb;
+
+    public function __construct($wpdb) {
+        $this->wpdb = $wpdb;
+    }
+
+    public function execute($query) {
+        return $this->wpdb->query($query);
+    }
+
+    public function get_results($query) {
+        return $this->wpdb->get_results($query);
+    }
+
+    public function get_var($query) {
+        return $this->wpdb->get_var($query);
+    }
+}
 
